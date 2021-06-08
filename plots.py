e43408da7915d9b29ca3170619b89fd85923ee7c
Danil Tyulmankov <2185856+dtyulman@users.noreply.github.com>
2021-06-07 19:17:55 -0700

    Nicer plotting

--- plots.py
+++ plots.py
@@ -29,9 +29,9 @@ def plot_loss_acc(loss, acc, test_loss=None, test_acc=None, iters=None,
 
 
 def plot_weights_mnist(W, max_n_rows=1024, plot_class_pixels=True, ax=None):
-    V_PX_PLT = V_PX_IMG = 28
-    H_PX_PLT = H_PX_IMG = 28
-    C_PX_IMG = 10
+    MNIST_VPIX = 28
+    MNIST_HPIX = 28
+    MNIST_CLASSES = 10
 
     if ax is None:
         fig, ax = plt.subplots()
@@ -45,33 +45,50 @@ def plot_weights_mnist(W, max_n_rows=1024, plot_class_pixels=True, ax=None):
         print(f'Plotting only the first {max_n_rows}/{W.shape[0]} entries')
         W = W[:max_n_rows]
 
-    if W.shape[1] == V_PX_IMG*H_PX_IMG + C_PX_IMG:
-        W_cls = W[:, -C_PX_IMG:]
-        W = W[:, :-C_PX_IMG]
-    elif W.shape[1] ==  V_PX_IMG*H_PX_IMG:
-        W_cls = None
-    else:
-        raise ValueError(f'Unexpected number of columns: shape={W.shape}')
+    img_list = rows_to_images(W, vpix=MNIST_VPIX, hpix=MNIST_HPIX, drop_last=MNIST_CLASSES)
+    grid = images_to_grid(img_list, vpad=1, hpad=1)
 
-    if plot_class_pixels:
-        H_PX_PLT = H_PX_IMG+1
-
-    r = int(np.ceil(np.sqrt(W.shape[0])))
-    c = int(np.round(np.sqrt(W.shape[0])))
-    W_plt = np.full((r*V_PX_PLT, c*H_PX_PLT), float('nan'))
-    for i in range(r):
-        for j in range(c):
-            idx = i*r + j
-            if idx >= W.shape[0]:
-                break
-            w = W[idx].reshape(V_PX_IMG, H_PX_IMG)
-            if plot_class_pixels:
-                wc = np.full((V_PX_PLT,1), float('nan'))
-                wc[:C_PX_IMG,:] = W_cls[idx].reshape(-1,1)
-                w = np.concatenate((w,wc), axis=1)
-            W_plt[i*V_PX_PLT:(i+1)*V_PX_PLT, j*H_PX_PLT:(j+1)*H_PX_PLT] = w
-
-    im = ax.imshow(W_plt, cmap='RdBu_r')
+    v = np.nanmax(np.abs(grid))
+    im = ax.imshow(grid, cmap='RdBu_r', vmin=-v, vmax=v)
     fig.colorbar(im)
+    ax.axis('off')
 
     return ax
+
+
+###############
+### HELPERS ###
+###############
+def rows_to_images(M, vpix=-1, hpix=-1, drop_last=0):
+    """Convert a matrix <M> with R rows to a list of R <vpix>-by-<hpix> matrices (e.g. images).
+    Optionally discard the <drop_last> entries of each row before reshaping it."""
+    if vpix==-1 and hpix==-1:
+        raise ValueError('Must specify at least one dimension of the image')
+    if drop_last > 0:
+        M = M[:,:-drop_last]
+    return [r.reshape(vpix,hpix) for r in M]
+
+
+def images_to_grid(img_list, rows=None, cols=None, vpad=0, hpad=0):
+    """Convert a list of matrices (e.g. images) to a <rows>-by-<cols> grid of images (i.e. one
+    large matrix). Optionally pad each matrix below/left with vpad/hpad rows/cols of NaNs"""
+    n_imgs = len(img_list)
+
+    if rows is None and cols is None:
+        rows = int(np.ceil(np.sqrt(n_imgs)))
+        cols = int(np.round(np.sqrt(n_imgs)))
+    elif rows is None and cols is not None:
+        rows = int(np.ceil(n_imgs/cols))
+    elif rows is not None and cols is None:
+        cols = int(np.ceil(n_imgs/rows))
+    assert rows*cols >= n_imgs
+
+    vpix = img_list[0].shape[0] + vpad
+    hpix = img_list[0].shape[1] + hpad
+    grid = np.full((rows*vpix, cols*hpix), fill_value=float('nan'))
+    for i,img in enumerate(img_list):
+        r = i//cols
+        c = i%cols
+        grid[r*vpix:(r+1)*vpix-vpad, c*hpix:(c+1)*hpix-hpad] = img
+
+    return grid
